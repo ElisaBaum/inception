b276d5d47b1e3079d89e6dfac34759ea735121a8
robinbuschmann <robinbuschmann@hotmail.de>
2019-06-11 21:08:02 +0200

    Removes error link from apollo

--- packages/client/src/core/apollo.ts
+++ packages/client/src/core/apollo.ts
@@ -1,7 +1,6 @@
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
-import {onError} from 'apollo-link-error';
 import {ApolloLink} from 'apollo-link';
 import {setContext} from 'apollo-link-context';
 import {getTokenId} from '../user/userSelectors';
@@ -9,15 +8,15 @@ import {store} from './store';
 
 export const client = new ApolloClient({
     link: ApolloLink.from([
-        onError(({graphQLErrors, networkError}) => {
-            if (graphQLErrors)
-                graphQLErrors.map(({message, locations, path}) =>
-                    console.log(
-                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-                    ),
-                );
-            if (networkError) console.log(`[Network error]: ${networkError}`);
-        }),
+        // onError(({graphQLErrors, networkError}) => {
+        //     if (graphQLErrors)
+        //         graphQLErrors.map(({message, locations, path}) =>
+        //             console.log(
+        //                 `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        //             ),
+        //         );
+        //     if (networkError) console.log(`[Network error]: ${networkError}`);
+        // }),
         setContext(() => {
             const tokenId = getTokenId(store.getState());
             if (tokenId) {
